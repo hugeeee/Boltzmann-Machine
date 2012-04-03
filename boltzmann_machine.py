d3ec3e3ce0eaab1f16b9c934c0ca9084729e0616
hugo <hugokinghall@gmail.com>
2012-04-03 15:26:25 +0100

    method added, not sure on the matrix for weights yet

--- boltzmann_machine.py
+++ boltzmann_machine.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
 
+import random
+
+binary = [0, 1]
+
+input_1 = [[-1, -1, -1, -1, -1, 1], [-1, -1, 1, -1, -1, -1], [1, -1, -1, -1, -1, -1]]
+
+examples = 0
+
+def num_examples(_input):
+	for i in _input:
+		examples += len(i)
+
+def activation(value):
+	z = 0
+	for i in range(neurons):
+		if i != value:
+			z += weights[value][i] * net[i]
+
+	return z
+
 print ("Please enter the number of neurons")
 
 user_input = raw_input('--> ') # gets input from the user
-neuron_values = []
+net = []
+weights = {} # hash to store weights for neurons
+dw = {}
+
+x = random.uniform(-1, 1)
+round(x, 2)
 
 neurons = int(user_input) # parses the input to an int
 
-for i in range (0, neurons):
-	print i
-	neuron_values.append(0)
+for i in range (neurons):
+	net.append(0)
+#	weights[i] = random.choice(binary)
+	weights[i] = [] # init with key mapping to a list
+	dw[i] = []
+	for x in range (neurons):
+		weights[i].append(random.choice(binary))
+
 
 print "done"
-
-for i in range (len(neuron_values)):
-	print neuron_values[i]
-
 
